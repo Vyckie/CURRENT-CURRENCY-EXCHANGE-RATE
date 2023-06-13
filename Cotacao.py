39a8213741c28a75860f581b5061a226458553d6
Vyckie <vyckie@aluno.ufrb.edu.br>
2023-06-13 16:22:56 -0300

    second version

--- Cotacao.py
+++ Cotacao.py
@@ -1,5 +1,5 @@
 import requests
-from tkinter import *
+import customtkinter
 
 def pegar_cotacoes():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
@@ -15,23 +15,23 @@ def pegar_cotacoes():
     Euro: {cotacao_euro}
     BTC: {cotacao_btc}'''
 
-    texto_cotações["text"]=texto
+    texto_cotações = customtkinter.CTkLabel(janela, text=texto)
+    texto_cotações.grid(column=0, row=2, padx=0, pady=0)
 
-
-janela = Tk()
+customtkinter.set_appearance_mode("Dark")
+janela = customtkinter.CTk()
 janela.title("COTAÇÃO ATUAL DAS MOEDAS")
-janela.geometry("300x300")
+janela.geometry("360x300")
+#janela.iconbitmap("cifrao.png") 
+janela.resizable(False, False)
 
-texto_orientação = Label(janela, text="CLIQUE ABAIXO PARA VIZUALIZAR AS COTAÇÕES")
+texto_orientação = customtkinter.CTkLabel(janela, text="CLIQUE ABAIXO PARA VIZUALIZAR AS COTAÇÕES", font=("Oswald", 14))
 texto_orientação.grid(column=0, row=0, padx=15, pady=20)
 
-botao = Button(janela, text="BUSCAR COTAÇÕES", command=pegar_cotacoes)
-botao.grid(column=0, row=1, padx=12, pady=15)
+botao = customtkinter.CTkButton(janela, text="BUSCAR COTAÇÕES", command=pegar_cotacoes)
+botao.grid(column=0, row=1 ,padx=12, pady=15)
 
-texto_cotações = Label(janela, text="")
-texto_cotações.grid(column=0, row=2,  padx=0, pady=0)
-
-texto_versao = Label(janela, text="@vyckie.2023")
+texto_versao = customtkinter.CTkLabel(janela, text="@vyckie.2023", font=("Courier New", 10))
 texto_versao.grid(column=0, row=3, padx=15, pady=10)
 
 janela.mainloop()
